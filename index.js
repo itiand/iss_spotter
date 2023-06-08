58f0663c682c4f94e02bceb0137c23c56dd63f80
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-08 18:42:53 -0400

    start ISS app - fetch my IP function complete

---
+++ index.js
@@ -0,0 +1,9 @@
+const { fetchMyIP } = require('./iss')
+
+// fetchMyIP((err, data) => {
+//   if (err) {
+//     console.log("It didn't work!", err);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , data);
+// });
